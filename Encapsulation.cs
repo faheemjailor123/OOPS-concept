ef81e7f3519872ffd8c9d8c029bc0a675f069a38
faheemjailor123 <93036039+faheemjailor123@users.noreply.github.com>
2021-10-26 07:49:42 -0700

    encap.cs

---
+++ Encapsulation.cs
@@ -0,0 +1,39 @@
+using System;
+  
+public class Encap {
+ 
+    private String studentName;
+    private int studentAge; 
+  
+    public String Name
+    {
+       get
+        {
+            return studentName;    
+        }
+          
+        set 
+        {
+            studentName = value;
+        }
+          
+    }
+     
+    public int Age
+    {
+          
+        get 
+        {
+            return studentAge;    
+        }
+          
+        set 
+        {
+            studentAge = value;
+        }
+          
+    }
+  
+      
+}
+  
